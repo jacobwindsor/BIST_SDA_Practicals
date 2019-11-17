859e0f7bc6dd853ddc26b146b51c617f7f16043c
Jacob Windsor <me@jcbwndsr.com>
2019-11-17 17:54:10 +0100

    Don't perform binomial for columns in data

--- Practical1/binomial.py
+++ Practical1/binomial.py
@@ -6,17 +6,14 @@ import numpy as np # Scientific computing
 from pathlib import Path
 from sample_from_simulated import sample_from_simulated
 
-data = pd.read_table(Path.cwd() / "Practical1/spider_web.txt", sep=' ')
-
-def binomial(column, sampleSize = 100, numSamples = 1000):
+def binomial(sampleSize = 100, numSamples = 1000):
     np.random.seed(10)
     distribution = np.random.binomial(1000, 0.5, sampleSize)
 
     sample_from_simulated(distribution, sampleSize, numSamples)
 
-for column in data.columns.delete(0):
-    print(f"Performing simulated binomial sampling for {column}.")
-    print("======================================================\n\n")
-    binomial(column)
-    binomial(column, 3, 10)
-    binomial(column, 10, 50)
+print(f"Performing simulated binomial sampling.")
+print("======================================================\n\n")
+binomial()
+binomial(3, 10)
+binomial(10, 50)
